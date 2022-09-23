5625a945fed51b989122857fb23374b2d85196d8
David1340 <davidnike80@gmail.com>
2022-09-22 21:28:26 -0300

    inserir o plot do manipulador

--- pioneer_7dof.py
+++ pioneer_7dof.py
@@ -10,6 +10,7 @@ from  math import pi
 import numpy as np
 from funcoes import matriz_homogenea,S
 from random import uniform
+import matplotlib.pyplot as plt
 
 def getDH_paramaters(q):
     base = 0.05
@@ -307,3 +308,138 @@ def getLengthElos():
 
 def getTypeJoints():
     return ['pivot','hinge','pivot','hinge','pivot','hinge','hinge']
+
+def plot_junta_revolucao(A,p,c,ax,h,r,cor = 'blue', offset = 0):
+    #A matriz de Rotação, p origem da junta no seu sistema de coordenadas, c eixo do
+
+    theta = np.arange(0,2*pi + 0.12,0.8) #theta de 0 a 2pi com passos de 0.1
+    if(c == 'z'):  
+        z = np.linspace(0,h,np.size(theta)) + offset#z de 0.1 a 0.1 com o numero de elementos iguais ao de theta
+        z, theta = np.meshgrid(z, theta) #transforma em vetores 2D por causa do plot de superficie
+        #[x,y,z].T = A@([x,y,z].T + p)
+        x = (r*np.cos(theta) + p[0,0])*A[0,0] + (r*np.sin(theta) + p[1,0])*A[0,1] + \
+         (z + p[2,0])*A[0,2] + np.ones_like(z)*A[0,3]
+        y = (r*np.cos(theta) + p[0,0])*A[1,0] + (r*np.sin(theta) + p[1,0])*A[1,1] + \
+         (z + p[2,0])*A[1,2] + np.ones_like(z)*A[1,3]
+        z = (r*np.cos(theta) + p[0,0])*A[2,0] + (r*np.sin(theta) + p[1,0])*A[2,1] + \
+         (z + p[2,0])*A[2,2] + np.ones_like(z)*A[2,3]
+        ax.plot_surface(x, y, z,color = cor, alpha = 1)
+        
+    elif(c == 'y'):
+        y = np.linspace(-h,h,np.size(theta)) + offset#z de 0.1 a 0.1 com o numero de elementos iguais ao de theta
+        y, theta = np.meshgrid(y, theta) #transforma em vetores 2D por causa do plot de superficie
+        #[x,y,z].T = A@([x,y,z].T + p)
+        x = (r*np.cos(theta) + p[0,0])*A[0,0] + (r*np.sin(theta) + p[2,0])*A[0,2] + \
+         (y + p[1,0])*A[0,1] + np.ones_like(y)*A[0,3]
+        z = (r*np.cos(theta) + p[0,0])*A[2,0] + (r*np.sin(theta) + p[2,0])*A[2,2] + \
+         (y + p[1,0])*A[2,1] + np.ones_like(y)*A[2,3]
+        y = (r*np.cos(theta) + p[0,0])*A[1,0] + (r*np.sin(theta) + p[2,0])*A[1,2] + \
+         (y + p[1,0])*A[1,1] + np.ones_like(y)*A[1,3]
+        ax.plot_surface(x, y, z,color = cor, alpha = 1)
+        
+    elif(c == 'x'):
+        x = np.linspace(-h,h,np.size(theta)) + offset#z de 0.1 a 0.1 com o numero de elementos iguais ao de theta
+        x, theta = np.meshgrid(x, theta) #transforma em vetores 2D por causa do plot de superficie
+        #[x,y,z].T = A@([x,y,z].T + p)
+        y = (r*np.cos(theta) + p[2,0])*A[1,2] + (r*np.sin(theta) + p[1,0])*A[1,1] + \
+         (x + p[0,0])*A[1,0] + np.ones_like(x)*A[1,3]
+        z = (r*np.cos(theta) + p[2,0])*A[2,2] + (r*np.sin(theta) + p[1,0])*A[2,1] + \
+         (x + p[0,0])*A[2,0] + np.ones_like(x)*A[2,3]
+        x = (r*np.cos(theta) + p[2,0])*A[0,2] + (r*np.sin(theta) + p[1,0])*A[0,1] + \
+         (x + p[0,0])*A[0,0] + np.ones_like(x)*A[0,3]
+        ax.plot_surface(x, y, z,color = cor, alpha = 1)
+
+#Função que plota o manipulador, quando recebe os pontos de interesse
+def plot(q,t,ax,fig):
+    #Pontos de interesse
+    p = np.array([[0,0,0,1]]).T #Base
+    p1_1 = np.array([[0,-0.05,0,1]]).T #junta1
+    p2_2 = p #junta2
+    p3_3 = np.array([[0,-0.075,0,1]]).T #junta3
+    p4_4 = p #junta4
+    p5_5 = np.array([[0,-0.0725,0,1]]).T #junta5
+    p6_6 = np.array([[-0.075,0,0,1]]).T #junta6
+    p7_7 = p #junta7
+
+    w = 0.05 #largura do efetuador
+    h = 0.025 #altura do efetuador
+    L = 0.075 #comprimento do elo que liga a junta 7 ao efetuador
+
+    #O efetuador será um u, para plotarmos um precisamos de 4 pontos
+    #topo esquerdo do u
+    
+    e1_7 = np.array([[0,-w/2,L + h,1]]).T
+    #base esquerda do u
+    e2_7 = np.array([[0,-w/2,L,1]]).T
+    #base direita do u
+    e3_7 = np.array([[0,w/2,L,1]]).T
+    #topo direito do u
+    e4_7 = np.array([[0,w/2,L+ h,1]]).T 
+
+    d,a,alpha,theta,p8_7 = getDH_paramaters(q)
+
+    #Calculando as matrizes homogêneas
+    A1 = matriz_homogenea(d[0],a[0],alpha[0],theta[0])
+    A2 = matriz_homogenea(d[1],a[1],alpha[1],theta[1])
+    A3 = matriz_homogenea(d[2],a[2],alpha[2],theta[2])
+    A4 = matriz_homogenea(d[3],a[3],alpha[3],theta[3])
+    A5 = matriz_homogenea(d[4],a[4],alpha[4],theta[4])
+    A6 = matriz_homogenea(d[5],a[5],alpha[5],theta[5])
+    A7 = matriz_homogenea(d[6],a[6],alpha[6],theta[6])
+
+    #Calculando os pontos de interesse no sistema Global
+    T1 = A1
+    T2 = T1@A2
+    T3 = T2@A3
+    T4 = T3@A4
+    T5 = T4@A5
+    T6 = T5@A6
+    T7 = T6@A7
+    p1_0 = T1@p1_1
+    p2_0 = T2@p2_2
+    p3_0 = T3@p3_3
+    p4_0 = T4@p4_4
+    p5_0 = T5@p5_5
+    p6_0 = T6@p6_6
+    p7_0 = T7@p7_7
+    p8_0 = T7@p8_7
+    e1_0 = T7@e1_7
+    e2_0 = T7@e2_7
+    e3_0 = T7@e3_7
+    e4_0 = T7@e4_7   
+   
+    #Plotando Elos 
+    ax.clear()   
+   
+    #Plotando efetuador
+    plt.plot([e1_0[0,0],e2_0[0,0],e3_0[0,0],e4_0[0,0],e3_0[0,0]]\
+             ,[e1_0[1,0],e2_0[1,0],e3_0[1,0],e4_0[1,0],e3_0[1,0]]\
+             ,[e1_0[2,0],e2_0[2,0],e3_0[2,0],e4_0[2,0],e3_0[2,0]],'blue')
+    
+    #Plotando objetivo    
+    ax.scatter(t[0,0],t[1,0],t[2,0],color = 'red')
+    
+    #Plotando Juntas e base   
+ 
+    plot_junta_revolucao(np.eye(4),p,'z',ax,0.025,0.025,'blue')
+    plot_junta_revolucao(T1,p1_1,'y',ax,0.05,0.025,'green',-0.025*0.5)
+    plot_junta_revolucao(T2,p2_2,'y',ax,0.025,0.025,'blue')
+    plot_junta_revolucao(T3,p3_3,'y',ax,0.05,0.025,'green')
+    plot_junta_revolucao(T4,p4_4,'y',ax,0.025,0.025,'blue')
+    plot_junta_revolucao(T5,p5_5,'y',ax,0.05,0.025,'green')
+    plot_junta_revolucao(T6,p6_6,'y',ax,0.025,0.025,'blue')
+    plot_junta_revolucao(T6,p6_6,'x',ax,0.5*0.025,0.025,'blue',1.5*0.025)
+    plot_junta_revolucao(T7,p7_7,'y',ax,0.025,0.025,'green')
+    plot_junta_revolucao(T7,p7_7,'z',ax,0.04,0.025,'green',0.025)
+
+    ax.set_xlabel('Eixo x(m)')
+    ax.set_ylabel('Eixo y(m)')
+    ax.set_zlabel('Eixo z(m)')
+
+    #titulo
+    plt.title('Pioneer 7DOF')
+    ax.set_xlim3d(-0.5,0.5)
+    ax.set_ylim3d(-0.5,0.5)
+    ax.set_zlim3d(0,0.5)
+    fig.canvas.draw() #mostra o plot
+    plt.pause(200)
