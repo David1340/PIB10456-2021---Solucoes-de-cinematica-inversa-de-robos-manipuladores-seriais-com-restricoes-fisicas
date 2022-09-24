2a6e82676f52e3f8daa6bd08db6e6d30a42f4f51
David1340 <davidnike80@gmail.com>
2022-09-24 13:16:28 -0300

    Experimentos focados no FRPSO

---
+++ ExperimentoB3.py
@@ -0,0 +1,86 @@
+#Autor David Oliveira.
+#Estudante de Engenharia Eletrônica da Universidade Federal de Sergipe-UFS.
+#Membro do Grupo de Pesquisa em Robotica da UFS-GPRUFS.
+#Experimentos apenas a posição .
+#Implementações feitas durante durante a iniciação científica intitulada:
+#PIB10456-2021 - Soluções de cinemática inversa de robôs manipuladores seriais com restrições físicas
+#Durante o período: PIBIC 2021/2022 (01/09/2021 a 31/08/2022).
+
+#Import das bibliotecas
+import numpy as np
+from random import uniform
+import sys
+import os
+import platform
+from funcoes import Esfera
+
+#Import diretorio
+diretorio_atual = os.getcwd()
+sys.path.append(diretorio_atual)
+if(platform.system == 'Windows'):
+    sys.path.append(diretorio_atual + '\Metodos')
+else:
+    sys.path.append(diretorio_atual + '/Metodos')
+
+#Import dos métodos
+from FRPSO import FRPSO
+from pioneer_7dof import *
+
+#Configurações do experimento
+Kmax = 1000
+erro_min = 0.001
+repeticoes = 1
+
+#parâmetros do manipulador
+qlim = getLimits() 
+n = getNumberJoints()
+q = np.zeros([n,1])
+
+kFRPSO = []
+
+tc = [0]
+mi = tc.copy()
+
+#obstáculos
+esferas = []
+esferas.append(Esfera(0.1,0.1,0.3,0.05))
+esferas.append(Esfera(-0.1,-0.1,0.3,0.05))
+esferas.append(Esfera(-0.1,0.1,0.3,0.05))
+esferas.append(Esfera(0.1,-0.1,0.3,0.05))
+
+esferas.append(Esfera(0.1,0.1,0.1,0.05))
+esferas.append(Esfera(-0.1,-0.1,0.1,0.05))
+esferas.append(Esfera(-0.1,0.1,0.1,0.05))
+esferas.append(Esfera(0.1,-0.1,0.1,0.05))
+
+esferas = []
+for i in range(repeticoes):
+    print('i:',i)
+
+    #Gerando a configuração inicial
+    for i2 in range(np.size(q)):
+        q[i2] = uniform(-qlim[i2],qlim[i2])
+
+    [posicaod,orientacaod] = random_pose()
+    
+    #posicaod = np.array([[0.15,0.15,0.3]]).T
+
+    #[erro,k] = FRPSO(posicaod,orientacaod,erro_min,Kmax,esferas)
+    [erro,k] = FRPSO(posicaod,orientacaod,erro_min,Kmax)
+    if(erro < erro_min):
+        kFRPSO.append(k)
+
+
+tc[0] = (len(kFRPSO)/repeticoes) * 100
+
+mi[0] = np.mean(kFRPSO)
+
+print(tc)
+print(np.round(mi,2))
+metodos = ["FRPSO"]
+arquivo = open("ExperimentoB3.txt", "w")
+arquivo.write("Metodos: " + str(metodos) + "\n")
+arquivo.write("tc: " + str(np.round(tc,2)) + "\n")
+arquivo.write("mi: " + str(np.round(mi,2))+ "\n")
+arquivo.write("Kmax: " + str(Kmax))
+arquivo.close()
